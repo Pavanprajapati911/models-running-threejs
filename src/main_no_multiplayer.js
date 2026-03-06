4b3197d0cffc93ff5913d062b1621feda1a0b5ae
Pavanprajapati911 <pavanprajapat635.com>
2026-03-06 17:52:16 +0530

    gooddd

--- src/main_no_multiplayer.js
+++ src/main_no_multiplayer.js
@@ -141,7 +141,7 @@ const localChar = new Character(
 
 const ROOM_MODEL = "/models/room.glb";
 
-const gridCols = 5;     // width
+const gridCols = 3;     // width
 const gridRows = 2;     // depth
 const spacing = 20;     // distance between rooms
 
